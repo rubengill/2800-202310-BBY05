71ebe789afbd0dbbc78f55da8c622f9cb98a3217
Markus Lum-De Guzman <markusldg@hotmail.com>
2023-05-11 16:12:30 -0700

    Reformatted folder structure and added server side code for node usage. Fixed profile picture functionality, added password change functionality.

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const session = require("express-session");
+const path = require('path');
+
+const app = express();
+
+// Serve static assets
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+
+
+// Serve login.html
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app/html/login.html'));
+});
+
+app.get('/main.html', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app/html/main.html'));
+});
+
+app.get('/settings.html', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app/html/settings.html'));
+});
+
+app.get('/userProfile.html', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app/html/userProfile.html'));
+});
+
+app.get('/login.html', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app/html/login.html'));
+});
+
+app.listen(3000, function () {
+    console.log('App is listening on port 3000!');
+  });
--- scripts/userProfile.js
+++ scripts/userProfile.js
@@ -29,37 +29,6 @@ chooseFileListener();
 
 chooseFileListener();
 
-function saveUserInfo() {
-  let fullName = document.getElementById("full-name").value;
-  let userEmail = document.getElementById("email").value;
-  let userMobile = document.getElementById("mobile").value;
-  let favoriteGenre = document.getElementById("genre").value;
-
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      var currentUser = db.collection("users").doc(user.uid);
-      var userID = user.uid;
-
-      // Update the user information in Firestore
-      currentUser
-        .update({
-          fullName: fullName,
-          email: userEmail,
-          mobile: userMobile,
-          favoriteGenre: favoriteGenre
-        })
-        .then(() => {
-          console.log("User information updated in Firestore for user:", userID);
-        })
-        .catch(error => {
-          console.error("Error updating user information:", error);
-        });
-    } else {
-      console.log("No user currently signed in.");
-    }
-  });
-}
-
 function readUserInfo() {
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
@@ -68,16 +37,16 @@ function readUserInfo() {
         .get()
         .then(doc => {
           if (doc.exists) {
-            var userInfo = doc.data()
+            var userInfo = doc.data();
             document.getElementById("full-name").value = userInfo.name || '';
-            document.getElementById("id").value = userInfo.id || '';
+            document.getElementById("id").value = userInfo.id || ''; // Updated ID here
             document.getElementById("email").value = userInfo.email || '';
             document.getElementById("genre").value = userInfo.favoriteGenre || '';
           } else {
             console.log("No user information found.");
             // Set input fields to empty
             document.getElementById("full-name").value = '';
-            document.getElementById("id").value = '';
+            document.getElementById("id").value = ''; // Updated ID here
             document.getElementById("email").value = '';
             document.getElementById("genre").value = '';
           }
@@ -90,11 +59,45 @@ function readUserInfo() {
       // Set input fields to empty
       document.getElementById("full-name").value = '';
       document.getElementById("email").value = '';
-      document.getElementById("mobile").value = '';
+      document.getElementById("id").value = ''; // Updated ID here
       document.getElementById("genre").value = '';
     }
   });
 }
+
+function saveUserInfo() {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+
+      // Retrieve the user information from the input fields
+      let fullName = document.getElementById("full-name").value;
+      let userEmail = document.getElementById("email").value;
+      let id = document.getElementById("id").value;
+      let favoriteGenre = document.getElementById("genre").value;
+
+      // Update the user information in Firestore
+      currentUser
+        .update({
+          name: fullName,
+          email: userEmail,
+          id: id,
+          favoriteGenre: favoriteGenre
+        })
+        .then(() => {
+          console.log("User information updated in Firestore for user:", userID);
+          readUserInfo();
+        })
+        .catch(error => {
+          console.error("Error updating user information:", error);
+        });
+    } else {
+      console.log("No user currently signed in.");
+    }
+  });
+}
+
 
 function saveUserPic() {
   firebase.auth().onAuthStateChanged(function (user) {
@@ -103,60 +106,68 @@ function saveUserPic() {
       var fileInput = document.getElementById("fileInput");
       var ImageFile = fileInput.files[0];
 
-      // Asynchronous call to put File Object onto Cloud Storage
-      storageRef
-        .put(ImageFile)
-        .then(function (snapshot) {
-          console.log("Uploaded to Cloud Storage.");
+      // Check if a file is selected
+      if (ImageFile) {
+        // Asynchronous call to put File Object onto Cloud Storage
+        storageRef
+          .put(ImageFile)
+          .then(function (snapshot) {
+            console.log("Uploaded to Cloud Storage.");
 
-          // Asynchronous call to get the download URL of the uploaded image
-          storageRef
-            .getDownloadURL()
-            .then(function (url) {
-              // Get the download URL of the uploaded image
+            // Asynchronous call to get the download URL of the uploaded image
+            storageRef
+              .getDownloadURL()
+              .then(function (url) {
+                // Get the download URL of the uploaded image
 
-              // Asynchronous call to save the download URL into Firestore
-              firebase
-                .firestore()
-                .collection("users")
-                .where("email", "==", user.email)
-                .get()
-                .then(function (querySnapshot) {
-                  querySnapshot.forEach(function (doc) {
-                    doc.ref
-                      .update({
-                        profilePic: url, // Save the download URL into the "users" collection
-                      })
-                      .then(function () {
-                        console.log("Added Profile Pic URL to Firestore.");
-                        document.getElementById("personalInfoFields").disabled = true;
-                      })
-                      .catch(function (error) {
-                        console.error("Error updating profile picture URL in Firestore:", error);
-                      });
+                // Asynchronous call to save the download URL into Firestore
+                firebase
+                  .firestore()
+                  .collection("users")
+                  .where("email", "==", user.email)
+                  .get()
+                  .then(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                      doc.ref
+                        .update({
+                          profilePic: url, // Save the download URL into the "users" collection
+                        })
+                        .then(function () {
+                          console.log("Added Profile Pic URL to Firestore.");
+                          populatePicture();
+                          fileInput.value = "";
+                        })
+                        .catch(function (error) {
+                          console.error("Error updating profile picture URL in Firestore:", error);
+                        });
+                    });
+                  })
+                  .catch(function (error) {
+                    console.error("Error querying user document in Firestore:", error);
                   });
-                })
-                .catch(function (error) {
-                  console.error("Error querying user document in Firestore:", error);
-                });
-            })
-            .catch(function (error) {
-              console.error("Error getting download URL from Firebase Storage:", error);
-            });
-        })
-        .catch(function (error) {
-          console.error("Error uploading image to Firebase Storage:", error);
-        });
+              })
+              .catch(function (error) {
+                console.error("Error getting download URL from Firebase Storage:", error);
+              });
+          })
+          .catch(function (error) {
+            console.error("Error uploading image to Firebase Storage:", error);
+          });
+      } else {
+        console.log("No file selected.");
+      }
     } else {
       console.log("No user currently signed in.");
     }
   });
 }
 
+
+
 function populatePicture() {
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      const currentUser = db.collection("users").doc(user.email);
+      const currentUser = db.collection("users").doc(user.uid);
 
       currentUser
         .get()
@@ -183,3 +194,28 @@ function populatePicture() {
 }
 
 populatePicture();
+
+
+function changePassword() {
+  var newPassword = document.getElementById("new-password").value;
+  var confirmPassword = document.getElementById("confirm-password").value;
+
+  // Check if the new password and confirm password match
+  if (newPassword !== confirmPassword) {
+    console.log("New password and confirm password do not match");
+    return;
+  }
+
+  var user = firebase.auth().currentUser;
+
+  // Update the password
+  user.updatePassword(newPassword)
+    .then(() => {
+      console.log("Password changed successfully");
+      // Redirect the user or show a success message
+    })
+    .catch(error => {
+      console.error("Error changing password:", error);
+      // Handle the error appropriately
+    });
+}
