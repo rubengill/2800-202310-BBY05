8b4737831a5850e5496556502e5abf824ab85c53
Ruben Gill <rgill288@my.bcit.ca>
2023-05-10 23:43:10 -0700

    added calculateScore.js script to the end of html file so it works. Created calculateScore.js which is a function that adds the users score from the skill assessment questionaire

--- scripts/calculateScore.js
+++ scripts/calculateScore.js
@@ -15,44 +15,60 @@ db.collection('questions').get().then((querySnapshot) => {
     displayQuestion(currentQuestionIndex);
   });
 
-  function displayQuestion(index) {
-    if (index < questions.length) {
-      const question = questions[index];
-      const questionText = document.getElementById('question-text');
-      const choicesContainer = document.getElementById('choices-container');
-      questionText.textContent = question.question;
-  
-      choicesContainer.innerHTML = '';
-      question.choices.forEach((choice, i) => {
-        const button = document.createElement('button');
-        button.textContent = choice.answer;
-        button.addEventListener('click', () => {
-          // Store the user's answer and its points
-          userAnswers.push({ answer: i, points: choice.points });
-  
-          // Show the "Next Question" button
-          document.getElementById('next-question').style.display = 'block';
-        });
-        choicesContainer.appendChild(button);
-      });
-    } else {
-      // Calculate the total score and display the results
-      calculateTotalScore();
-    }
-  }
+  // Displays the questions from the question collection
+function displayQuestion(index) {
+  // Ensures that index is less than the array length 
+  if (index < questions.length) {
+    // If question index is in bounds, retrieve appropiate DOM elements
+    const question = questions[index];
+    const questionText = document.getElementById('question-text');
+    const choicesContainer = document.getElementById('choices-container');
+    // Update the question text content with the question text
+    questionText.textContent = question.question;
 
-  document.getElementById('next-question').addEventListener('click', () => {
-    currentQuestionIndex++;
-    document.getElementById('next-question').style.display = 'none';
-    displayQuestion(currentQuestionIndex);
+    // Remove any pre existing choices
+    choicesContainer.innerHTML = '';
+
+    // Loop through the questions collection 
+    question.choices.forEach((choice, i) => {
+      // Create a new button for each question 
+      const button = document.createElement('button');
+      button.textContent = choice.answer;
+      // Add an event listener to the button to store the user's answer and the points when it is clicked
+      button.addEventListener('click', () => {
+        // Store the user's answer and its points as an object in the "userAnswers" array
+        userAnswers.push({ answer: i, points: choice.points });
+
+        // Show the "Next Question" button
+        document.getElementById('next-question').style.display = 'block';
+      });
+      // Append the button to the choices container
+      choicesContainer.appendChild(button);
+    });
+  } else {
+    // If the index is out of bounds, calculate the total score and display the results
+    calculateTotalScore();
+  }
+}
+
+
+  //Add an event listener to the "Next Question" button
+document.getElementById('next-question').addEventListener('click', () => {
+  //increments the current question index by 1
+  currentQuestionIndex++;
+  //hides the "Next Question" button, and displays the next question
+  document.getElementById('next-question').style.display = 'none';
+  displayQuestion(currentQuestionIndex);
+});
+
+//Calculates the total score
+function calculateTotalScore() {
+  let totalScore = 0;
+  //iterate through the userAnswers array and add up the points for each answer
+  userAnswers.forEach((answer) => {
+    totalScore += answer.points;
   });
 
-  function calculateTotalScore() {
-    let totalScore = 0;
-    userAnswers.forEach((answer) => {
-      totalScore += answer.points;
-    });
-  
-    // Display the total score or navigate to the results page
-    console.log('Total score:', totalScore);
-  }
+  // Display the total score 
+  console.log('Total score:', totalScore);
+}
