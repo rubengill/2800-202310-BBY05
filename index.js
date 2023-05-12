c6cd6283c48ea0f9bd6d2fc032f28d0ce2c55b4e
Ruben Gill <rgill288@my.bcit.ca>
2023-05-12 12:58:24 -0700

    edited dirname variable in index.js to contains the underscores.

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ const path = require('path');
 const app = express();
 
 //Creating remote paths, for our static files
-app.use('/scripts', express.static(path.join(dirname, 'scripts')));
-app.use('/css', express.static(path.join(dirname, 'public/css')));
-app.use('/img', express.static(path.join(dirname, 'public/img')));
-app.use('/template', express.static(path.join(dirname, 'template')));
-app.use('/data', express.static(path.join(dirname, 'app/data')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use('/template', express.static(path.join(__dirname, 'template')));
+app.use('/data', express.static(path.join(__dirname, 'app/data')));
 
 // Serve login.html
 app.get('/', function (req, res) {
-    res.sendFile(path.join(dirname, 'app/html/login.html'));
+    res.sendFile(path.join(__dirname, 'app/html/login.html'));
 });
 
 app.get('/main.html', function (req, res) {
-    res.sendFile(path.join(dirname, 'app/html/main.html'));
+    res.sendFile(path.join(__dirname, 'app/html/main.html'));
 });
 
 app.get('/settings.html', function (req, res) {
-    res.sendFile(path.join(dirname, 'app/html/settings.html'));
+    res.sendFile(path.join(__dirname, 'app/html/settings.html'));
 });
 
-app.get('/app/html/userProfile.html', function (req, res) {
-    res.sendFile(path.join(dirname, 'app/html/userProfile.html'));
+app.get('/userProfile.html', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app/html/userProfile.html'));
 });
 
 app.get('/questions.html', function (req, res) {
-    res.sendFile(path.join(dirname, 'app/html/questions.html'));
+    res.sendFile(path.join(__dirname, 'app/html/questions.html'));
 });
 
 app.get('/login.html', function (req, res) {
