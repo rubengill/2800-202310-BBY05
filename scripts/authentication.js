e1652d7d977e238fa7c167f6c1d9728773e24681
Ruben Gill <rgill288@my.bcit.ca>
2023-05-12 11:40:15 -0700

    edied paths

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -28,7 +28,7 @@ var uiConfig = {
           })
           .then(function () {
             console.log("New user added to firestore");
-            window.location.assign("/app/html/main.html"); //re-direct to main.html after signup
+            window.location.assign("/main.html"); //re-direct to main.html after signup
           })
           .catch(function (error) {
             console.log("Error adding new user: " + error);
@@ -46,7 +46,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: "popup",
-  signInSuccessUrl: "/app/html/main.html",
+  signInSuccessUrl: "/main.html",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
