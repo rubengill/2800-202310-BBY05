6e86fa61c5a45038f4e232ed0feefcaca3358ef5
Ruben Gill <rgill288@my.bcit.ca>
2023-05-16 19:16:00 -0700

    added fetchandDisplaySkillLevel function which will calculate and display to the user their calculated skill

--- scripts/SkillAssessment.js
+++ scripts/SkillAssessment.js
@@ -117,3 +117,26 @@ function calculateSkillLevel(score) {
   return skillLevel;
 }
 
+function fetchAndDisplaySkillLevel() {
+  // Get the current user's UID
+  const uid = firebase.auth().currentUser.uid;
+
+  // Fetch the user's data from Firestore
+  db.collection('users').doc(uid).get()
+    .then((doc) => {
+      if (doc.exists) {
+        // Retrieve the skill level from the document
+        const skillLevel = doc.data().skillLevel;
+        
+        // Set the text content of the "skill" div to the skill level
+        document.getElementById('skill').textContent = skillLevel;
+      } else {
+        console.error("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting document:", error);
+    });
+}
+
+
