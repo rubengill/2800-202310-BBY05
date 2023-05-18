1baf1ca77ec6dbd25fb756ed831a306478ab48eb
Joseph Louwerse <josephspam27@gmail.com>
2023-05-17 20:32:42 -0700

    Big changes to js files; one card, can navigate.

--- scripts/btnsToPractice.js
+++ scripts/btnsToPractice.js
@@ -1,20 +1,61 @@
 function addButton() {
     const containerIds = ["frmTask1"];
-    
-    // //testing if concat string gets the form corr to curr task
-    // var myForm = "frmTask" + currentTask;
-    // const testContainer = document.getElementById(myForm);
-    // const testTopSection = testContainer.querySelector("#topSection");
-    // testTopSection.innerHTML += `<button>BALLS</button>`;
 
-    containerIds.forEach((containerId) => {
-        const container = document.getElementById(containerId);
-        const topSection = container.querySelector(".topSection");
-        topSection.innerHTML += "<button>s</button>";
-        console.log(currentTask);
-    });
+    const container = document.getElementById(myContainer);
+    const topSection = container.querySelector(".topSection");
+    topSection.innerHTML =
+        `<h3> TASK ${currentTask} </h3> ` +
+        "<button onclick='previousTask(event);'>previous</button>" +
+        "<button>skip</button>" +
+        "<button onclick='nextTask(event);'>next</button>";
 }
 
-window.onload = function() {
+window.onload = function () {
     addButton();
-  };
+};
+
+function previousTask(event) {
+    event.preventDefault();
+    if (currentTask != FIRST_TASK) {
+        currentTask--;
+    }
+    updatePage();
+}
+
+//Todo skip task fn
+
+function nextTask(event) {
+    event.preventDefault();
+    if (currentTask != LAST_TASK) {
+        currentTask++;
+    }
+    updatePage();
+}
+
+function updatePage() {
+    updateMyContainer();
+
+    //updateSong();
+    displaySong();
+    addButton();
+    currContainer = "cardTask" + currentTask;
+    const container = document.getElementById(currContainer);
+
+    // container.style = "display: none;";
+//---------might be useful if you need to check the css of an element---------------------
+    // if ($(container).css("display") == "none") {
+    //     // true
+    // }
+    container.style = "display: block;";
+
+    for (let i = 1; i <= LAST_TASK; i++) {
+        //show this task card
+        if (i != currentTask) {
+            currContainer = "cardTask" + i;
+            const others = document.getElementById(currContainer);
+            others.style = "display: none;";
+        }
+        //hide all others
+    }
+    console.log("------updatePage() finished------")
+}
--- scripts/getRanSong.js
+++ scripts/getRanSong.js
@@ -1,77 +1,116 @@
-const databaseRef = db.collection("database");
-databaseRef.get().then((querySnapshot) => {
-    const songs = [];
-    querySnapshot.forEach((doc) => {
-      const song = doc.data();
-      songs.push(song);
-    });
-  
-    // Select a random song
-    const randomIndex = Math.floor(Math.random() * songs.length);
-    const randomSong = songs[randomIndex];
-  
-    // Display the song on your website
-    displaySong(randomSong);
-    //displaySong
-});
+// const tempDB = db.collection("temp_small_database");
+// tempDB.get().then((querySnapshot) => {
+//     var docRef = db.collection("temp_small_database").doc("document_0");
+//     docRef
+//         .get()
+//         .then((doc) => {
+//                 console.log("small data:", doc.data());
+//         })
+//         .catch((error) => {
+//             console.log("Error getting document:", error);
+//         });
+// });
+
+//const databaseRef = db.collection("database");
+const songs = [];
+function updateSong() {
+    //databaseRef.get().then((querySnapshot) => {
+    const numSongsPulled = 30;
+    //no 642nd song
+
+    for (let i = 0; i < LAST_TASK; i++) {
+        var randomDocNum = "document_" + Math.floor(Math.random() * 849);
+
+        var docRef = db.collection("database").doc(randomDocNum);
+
+        docRef
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    console.log("Document data:", doc.data());
+                    const song = doc.data();
+                    songs.push(song);
+
+                    // // Select a random song
+                    // const randomIndex = Math.floor(
+                    //     Math.random() * songs.length
+                    // );
+                    // const randomSong = songs[randomIndex];
+
+                    // // Display the song on your website
+                    // displaySong(randomSong);
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document: " + randomDocNum + "!");
+                }
+            })
+            .catch((error) => {
+                console.log("Error getting document:", error);
+            });
+    }
+    //});
+}
+updateSong();
+
+function displaySong() {
+    const container = document.getElementById(myContainer);
+    const bottomSection = container.querySelector(".bottomSection");
+    const song = songs[currentTask - 1];//tasks go 1-5, songs[] go 0-4
+
+    bottomSection.innerHTML = `
+    <h3>${song["Song Name"]}</h3>
+    <p>Artist: ${song.Artist}</p>
+    <p>Difficulty: ${song.Difficulty}</p>
+    <!-- Add additional song details as needed -->
+    `;
+    console.log("------displaySong() done----------")
+}
 
 
-function displaySong(song) {
-    const containerIds = ["frmTask1", "frmTask2", "frmTask3", "frmTask4", "frmTask5"];
-    
-    containerIds.forEach((containerId) => {
-      const container = document.getElementById(containerId);
-      container.innerHTML += `
-        <h2>${song['Song Name']}</h2>
-        <p>Artist: ${song.Artist}</p>
-        <p>Album: ${song.Difficulty}</p>
-        <!-- Add additional song details as needed -->
-      `;
-    });
-  }
+
 //   function displayRandomSongs() {
 //     const containerIds = ["frmTask1", "frmTask2", "frmTask3", "frmTask4", "frmTask5"];
 //     const songs = [];
-  
+
 //     // Check if songs are already generated for today
 //     // const storedSongs = localStorage.getItem('randomSongs');
 //     // const storedTimestamp = localStorage.getItem('randomSongsTimestamp');
-  
+
 //     // const currentTimestamp = new Date().toISOString().slice(0, 10);
-  
+
 //     // if (storedSongs && storedTimestamp === currentTimestamp) {
 //     //   songs.push(...JSON.parse(storedSongs));
-//     // } 
+//     // }
 //     // else {
 //       // Generate random songs
 //       const db = firebase.firestore();
 //       const databaseRef = db.collection("database");
-  
+
 //       databaseRef.get().then((querySnapshot) => {
 //         querySnapshot.forEach((doc) => {
 //           const song = doc.data();
 //           songs.push(song);
 //         });
-  
+
 //         // Shuffle the songs array
 //         for (let i = songs.length - 1; i > 0; i--) {
 //           const j = Math.floor(Math.random() * (i + 1));
 //           [songs[i], songs[j]] = [songs[j], songs[i]];
 //         }
-  
+
 //         // Store the generated songs and timestamp in local storage
 //         localStorage.setItem('randomSongs', JSON.stringify(songs));
 //         localStorage.setItem('randomSongsTimestamp', currentTimestamp);
-  
+
 //         displaySongsInContainers();
 //       });
 //     // }
-  
+
 //     function displaySongsInContainers() {
 //       containerIds.forEach((containerId, index) => {
 //         const container = document.getElementById(containerId);
 //         const song = songs[index];
-  
+
 //         container.innerHTML += `
 //             <h2>${song['Song Name']}</h2>
 //             <p>Artist: ${song.Artist}</p>
@@ -80,8 +119,8 @@ function displaySong(song) {
 //         `;
 //       });
 //     }
-  
+
 //     displaySongsInContainers();
 //   }
-  
+
 //   displayRandomSongs();
