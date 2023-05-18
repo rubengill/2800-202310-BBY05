34fd5c69f606b8c4b591b1b3fd080fe6ecec9844
jefferymj <jefferymjoseph@gmail.com>
2023-05-18 16:15:40 -0700

    fixed paths

--- index.js
+++ index.js
@@ -16,23 +16,23 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/html/index.html'));
 });
 
-app.get('/main.html', function (req, res) {
+app.get('/main', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/html/main.html'));
 });
 
-app.get('/settings.html', function (req, res) {
+app.get('/settings', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/html/settings.html'));
 });
 
-app.get('/userProfile.html', function (req, res) {
+app.get('/userProfile', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/html/userProfile.html'));
 });
 
-app.get('/questions.html', function (req, res) {
+app.get('/questions', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/html/questions.html'));
 });
 
-app.get('/login.html', function (req, res) {
+app.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/html/login.html'));
 });
 
