0f4c554793980a9d0034b9c7e94803821bef6a48
Ruben Gill <rgill288@my.bcit.ca>
2023-05-17 18:45:03 -0700

    added further code to console.log various points throughout the function to further determine where the error is occuring

--- scripts/randomSong.js
+++ scripts/randomSong.js
@@ -6,6 +6,14 @@ firebase.auth().onAuthStateChanged(async (user) => {
         // Call getRandomSongs on current user
         const songs = await getRandomSongs(uid);
         console.log(songs);
+
+        // Log each song's fields
+        for (let song of songs) {
+            console.log(song["Song Name"]); // Access the "Song Name" field
+            console.log(song["Artist"]); // Access the "Artist" field
+            console.log(song["Difficulty"]); // Access the "Difficulty" field
+            console.log(song["Random"]); // Access the "Random" field
+        }
     } else {
         // No user is signed in.
         console.log('No user is signed in.');
@@ -14,17 +22,18 @@ firebase.auth().onAuthStateChanged(async (user) => {
 
 
 async function getRandomSongs(uid) {
-    console.log('getRandomSongs uid:', uid);
+    console.log('getRandomSongs uid:', uid); // Log the uid at the start of the function
 
     // Fetch the user's data from Firestore
     let skillLevel;
     await db.collection('users').doc(uid).get()
         .then((doc) => {
-            console.log('doc data for uid ' + uid + ':', doc.data());
+            console.log('doc data for uid ' + uid + ':', doc.data()); // Log the document data
 
             if (doc.exists) {
                 // Retrieve the skill level from the document
                 skillLevel = doc.data().skillLevel;
+                console.log('skillLevel:', skillLevel); // Log the skill level
             } else {
                 console.error("No such document!");
             }
@@ -35,27 +44,36 @@ async function getRandomSongs(uid) {
     let songs = [];
     for (let i = 0; i < NUM_SONGS; i++) {
         const random = Math.random();
-        const songDoc = await db.collection('database')
-            .where('Difficulty', '==', skillLevel)
-            .where('Random', '>=', random)
-            .orderBy('Random')
-            .limit(1)
-            .get();
+        try {
+            const songDoc = await db.collection('database')
+                .where('Difficulty', '==', skillLevel)
+                .where('Random', '>=', random)
+                .orderBy('Random')
+                .limit(1)
+                .get();
 
-        if (!songDoc.empty) {
-            songs.push(songDoc.docs[0].data());
+            console.log('songDoc:', songDoc); // Log the song document
+
+            if (!songDoc.empty) {
+                const songData = songDoc.docs[0].data();
+                console.log('songData:', songData); // Log the song data
+                songs.push(songData);
+            }
+        } catch (error) {
+            console.error('Error fetching song:', error); // Log any errors
         }
     }
 
     return songs;
 }
 
+
 //Test to see if it correctly pulls songs by logging it to the console 
-getRandomSongs().then(songs => {
-    for (let song of songs) {
-        console.log(song["Song Name"]); // Access the "Song Name" field
-        console.log(song["Artist"]); // Access the "Artist" field
-        console.log(song["Difficulty"]); // Access the "Difficulty" field
-        console.log(song["Random"]); // Access the "Random" field
-    }
-});
+// getRandomSongs().then(songs => {
+//     for (let song of songs) {
+//         console.log(song["Song Name"]); // Access the "Song Name" field
+//         console.log(song["Artist"]); // Access the "Artist" field
+//         console.log(song["Difficulty"]); // Access the "Difficulty" field
+//         console.log(song["Random"]); // Access the "Random" field
+//     }
+// });
