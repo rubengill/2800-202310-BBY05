b1d0f6be82fdbd0282a271347abc2a6636edf54f
Markus Lum-De Guzman <markusldg@hotmail.com>
2023-05-18 18:44:50 -0700

    Social.js My Friends information pulls from users collection to update friends sub collection on page load.

--- scripts/social.js
+++ scripts/social.js
@@ -6,8 +6,8 @@ function createFriendCard(friendData) {
       <div class="card-body">
         <h5 class="card-title">${friendData.name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${friendData.email}</h6>
-        ${friendData.favoriteGenre ? `<p class="card-text">Favorite Genre: ${friendData.favoriteGenre}</p>` : ''}
-        ${friendData.streak && friendData.streak.count > 0 ? `<p class="card-text">Streak: ${friendData.streak.count} 🔥</p>` : ''}
+        ${friendData.favoriteGenre ? `<p class="card-text favorite-genre">Favorite Genre: ${friendData.favoriteGenre}</p>` : ''}
+        ${friendData.streak && friendData.streak.count > 0 ? `<p class="card-text streak">Streak: ${friendData.streak.count} 🔥</p>` : ''}
       </div>
     </div>
   `;
@@ -15,19 +15,81 @@ function createFriendCard(friendData) {
   return friendCard;
 }
 
-// Process a Firestore friend document, create a friend card, and add it to the "current-friends" section of the page
-function processFriendDocument(doc) {
-  // Extract friend data from the Firestore document
-  var friendData = doc.data();
+// Process a Firestore friend document, update the corresponding friend card in the "current-friends" section of the page
+function processFriendDocument(change) {
+  console.log('Processing friend document change:', change);
 
-  // Create a new friend card element using the friend card template and friend data
-  var friendCard = createFriendCard(friendData);
+  if (change.type === 'added' || change.type === 'modified') {
+    // Create a new friend card element using the friend data
+    var friendData = change.doc.data();
+    console.log('Friend data:', friendData);
+    var friendCard = createFriendCard(friendData);
 
-  // Add the friend card to the "current-friends" section of the page
-  document.getElementById('current-friends').appendChild(friendCard);
+    // Set the ID of the friend card to the document ID
+    friendCard.id = change.doc.id;
+
+    if (change.type === 'added') {
+      // Add the friend card to the "current-friends" section of the page
+      console.log('Adding friend card to the DOM:', friendCard);
+      document.getElementById('current-friends').appendChild(friendCard);
+    } else if (change.type === 'modified') {
+      // Update the friend card with the modified data
+      var existingFriendCard = document.getElementById(change.doc.id);
+      if (existingFriendCard) {
+        // Update the friend's name
+        existingFriendCard.querySelector('.card-title').textContent = friendData.name;
+        // Update the friend's email
+        existingFriendCard.querySelector('.card-subtitle').textContent = friendData.email;
+
+        // Update the favorite genre if available
+        var favoriteGenreElement = existingFriendCard.querySelector('.favorite-genre');
+        if (favoriteGenreElement) {
+          if (friendData.favoriteGenre) {
+            favoriteGenreElement.textContent = 'Favorite Genre: ' + friendData.favoriteGenre;
+            favoriteGenreElement.style.display = 'block';
+          } else {
+            favoriteGenreElement.style.display = 'none';
+          }
+        }
+
+        // Fetch the streak count from the 'streak' subcollection
+        db.collection('users')
+          .doc(change.doc.id) // Assuming friend's document ID is the same as their user ID
+          .collection('streak')
+          .doc('streak')
+          .get()
+          .then(function(streakDoc) {
+            if (streakDoc.exists) {
+              // Update the streak count if available
+              var streakElement = existingFriendCard.querySelector('.streak');
+              if (streakElement) {
+                var streakCount = streakDoc.data().count || 0;
+                if (streakCount > 0) {
+                  streakElement.textContent = 'Streak: ' + streakCount + ' 🔥';
+                  streakElement.style.display = 'block';
+                } else {
+                  streakElement.style.display = 'none';
+                }
+              }
+            }
+          })
+          .catch(function(error) {
+            console.error('Error fetching streak data:', error);
+          });
+      }
+    }
+  } else if (change.type === 'removed') {
+    // Remove friend card from the DOM
+    var friendCard = document.getElementById(change.doc.id);
+    if (friendCard) {
+      friendCard.parentNode.removeChild(friendCard);
+    }
+  }
 }
 
-// Get the user's current friends from Firestore and update the friend information
+
+
+
 function updateFriendInformation(userId) {
   var currentFriendsContainer = document.getElementById('current-friends');
 
@@ -36,30 +98,74 @@ function updateFriendInformation(userId) {
     currentFriendsContainer.removeChild(currentFriendsContainer.firstChild);
   }
 
-  // Retrieve and append updated friend information
+  // Fetch the friend data from Firestore
+  db.collection('users')
+    .doc(userId)
+    .collection('friends')
+    .onSnapshot(function (querySnapshot) {
+      querySnapshot.docChanges().forEach(function (change) {
+        if (change.type === 'added' || change.type === 'modified') {
+          // Process each friend document change and update the corresponding friend card
+          processFriendDocument(change);
+        } else if (change.type === 'removed') {
+          // Remove friend card from the DOM
+          var friendCard = document.getElementById(change.doc.id);
+          if (friendCard) {
+            friendCard.parentNode.removeChild(friendCard);
+          }
+        }
+      });
+    }, function (error) {
+      console.error('Error getting current friends:', error);
+    });
+
+  // Update friend documents with the latest information
   db.collection('users')
     .doc(userId)
     .collection('friends')
     .get()
     .then(function (querySnapshot) {
-      // Loop through the friend documents
       querySnapshot.forEach(function (doc) {
-        // Process each friend document and create a friend card
-        processFriendDocument(doc);
+        var friendData = doc.data();
+        db.collection('users')
+          .where('email', '==', friendData.email)
+          .get()
+          .then(function (userQuerySnapshot) {
+            if (!userQuerySnapshot.empty) {
+              var userData = userQuerySnapshot.docs[0].data();
+              db.collection('users')
+                .doc(userId)
+                .collection('friends')
+                .doc(doc.id)
+                .set(userData, { merge: true })  // Update friend document with latest information
+                .then(function () {
+                  console.log('Friend document updated:', doc.id);
+                })
+                .catch(function (error) {
+                  console.error('Error updating friend document:', error);
+                });
+            }
+          })
+          .catch(function (error) {
+            console.error('Error getting user data:', error);
+          });
       });
     })
     .catch(function (error) {
-      console.error('Error getting current friends: ', error);
+      console.error('Error getting current friends:', error);
     });
 }
 
-// Call the updateFriendInformation function with the user's ID when the page is loaded
-document.addEventListener('DOMContentLoaded', function () {
-  // Get the user's ID after the Firebase authentication is ready
-  firebase.auth().onAuthStateChanged(function (user) {
+
+// Load friend information when the page is loaded
+function loadFriendInformation() {
+  firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       var userId = user.uid;
       updateFriendInformation(userId);
     }
   });
-});
+}
+
+// Call the loadFriendInformation function when the page is loaded
+window.addEventListener('load', loadFriendInformation);
