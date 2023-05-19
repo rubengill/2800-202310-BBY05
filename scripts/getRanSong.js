c24db34621d81f803b9693d5dca8e117b038c6a6
Joseph Louwerse <josephspam27@gmail.com>
2023-05-19 04:32:08 -0700

    Added easter egg

--- scripts/getRanSong.js
+++ scripts/getRanSong.js
@@ -12,38 +12,130 @@
 // });
 
 //const databaseRef = db.collection("database");
-const songs = [];
-function updateSong() {
-    const numSongsPulled = 30;
-    const songPromises = [];
+// const songs = [];
+// function updateSong() {
+//     const numSongsPulled = 30;
+//     const songPromises = [];
   
-    for (let i = 0; i < LAST_TASK; i++) {
-      var randomDocNum = "document_" + Math.floor(Math.random() * 849);
+//     for (let i = 0; i < LAST_TASK; i++) {
+//       var randomDocNum = "document_" + Math.floor(Math.random() * 849);
   
-      var docRef = db.collection("database").doc(randomDocNum);
+//       var docRef = db.collection("database").doc(randomDocNum);
   
-      const songPromise = docRef.get().then((doc) => {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          const song = doc.data();
-          songs.push(song);
-        } else {
-          console.log("No such document: " + randomDocNum + "!");
-        }
-      }).catch((error) => {
-        console.log("Error getting document:", error);
-      });
+//       const songPromise = docRef.get().then((doc) => {
+//         if (doc.exists) {
+//           console.log("Document data:", doc.data());
+//           const song = doc.data();
+//           songs.push(song);
+//         } else {
+//           console.log("No such document: " + randomDocNum + "!");
+//         }
+//       }).catch((error) => {
+//         console.log("Error getting document:", error);
+//       });
   
-      songPromises.push(songPromise);
-    }
+//       songPromises.push(songPromise);
+//     }
   
-    Promise.all(songPromises).then(() => {
-      displaySong();
-    });
-  }
-updateSong();
+//     Promise.all(songPromises).then(() => {
+//       displaySong();
+//     });
+//   }
+// updateSong();
 
-function displaySong() {
+firebase.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+      // Get UID if user is signed in 
+      const uid = user.uid;
+      console.log(uid)
+      // Call getRandomSongs on current user
+      const songs = await getRandomSongs(uid);
+      displaySong(songs);
+      console.log(songs);
+
+      // Log each song's fields
+      for (let song of songs) {
+          console.log('Song Name: ' + song["Song Name"]); // Access the "Song Name" field
+          console.log('Artist: ' + song["Artist"]); // Access the "Artist" field
+          console.log('Difficulty: ' + song["Difficulty"]); // Access the "Difficulty" field
+      }
+  } else {
+      // No user is signed in.
+      console.log('No user is signed in.');
+  }
+});
+
+
+async function getRandomSongs(uid) {
+  console.log('getRandomSongs uid:', uid); // Log the uid at the start of the function
+
+  // Fetch the user's data from Firestore
+  let skillLevel;
+  await db.collection('users').doc(uid).get()
+      .then((doc) => {
+          console.log('doc data for uid ' + uid + ':', doc.data()); // Log the document data
+
+          if (doc.exists) {
+              // Retrieve the skill level from the document
+              skillLevel = doc.data().skillLevel;
+              console.log('skillLevel:', skillLevel); // Log the skill level
+          } else {
+              console.error("No such document!");
+          }
+      });
+
+  // Check if the user already has a songs subcollection
+  const songsCollection = db.collection('users').doc(uid).collection('songs');
+  const songsSnapshot = await songsCollection.get();
+
+  if (!songsSnapshot.empty) {
+      // The user already has songs, so log them and return them
+      let songs = songsSnapshot.docs.map(doc => doc.data());
+      console.log('Existing songs:', songs);
+      return songs;
+  }
+
+  // The user doesn't have any songs yet, so generate some
+  const NUM_SONGS = 5;
+  let songs = [];
+  for (let i = 0; i < NUM_SONGS; i++) {
+      const random = Math.random();
+      try {
+          const songDoc = await db.collection('database')
+              .where('Difficulty', '==', skillLevel)
+              .where('Random', '>=', random)
+              .orderBy('Random')
+              .limit(1)
+              .get();
+
+          console.log('songDoc:', songDoc); // Log the song document
+
+          if (!songDoc.empty) {
+              const songData = songDoc.docs[0].data();
+              console.log('songData:', songData); // Log the song data
+
+              // Add Song Name, Artist and Difficulty to songs array
+              songs.push({
+                  "Song Name": songData["Song Name"],
+                  "Artist": songData["Artist"],
+                  "Difficulty": songData["Difficulty"]
+              });
+
+              // Add the song to the user's songs subcollection
+              // Use the song's ID as the document ID
+              await songsCollection.doc(songData["Song Name"]).set(songData);
+          }
+      } catch (error) {
+          console.error('Error fetching song:', error); // Log any errors
+      }
+  }
+
+  return songs;
+}
+
+
+
+function displaySong(songs) {
     const container = document.getElementById(myContainer);
     const bottomSection = container.querySelector(".bottomSection");
     const song = songs[currentTask - 1];//tasks go 1-5, songs[] go 0-4
